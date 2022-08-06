49d62fbdb11fb3aaa7bdfbb453270122d13a9090
Shivam-77 <tiwarishivam77777@gmail.com>
2022-08-06 23:23:09 +0530

    Add

--- src/Array.java
+++ src/Array.java
@@ -2,7 +2,7 @@ public class Array
 {
     public static void main(String[] args)
     {
-        int[] arr={1,2,2,3,3,2,1,4};
+        int[] arr={4,2,2,3,3,2,1,4};
         for(int i=0;i< arr.length;i++){
             int c=0;
             for(int j=0;j< arr.length;j++){
---
+++ src/Wipro_Project.java
@@ -0,0 +1,3 @@
+public class Wipro_Project
+{
+}
