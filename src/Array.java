1895412739720a60f35fbe37f7c4cdf629c5b933
Shivam-77 <tiwarishivam77777@gmail.com>
2022-08-06 21:39:59 +0530

    Add

---
+++ src/Array.java
@@ -0,0 +1,18 @@
+public class Array
+{
+    public static void main(String[] args)
+    {
+        int[] arr={1,2,2,3,3,2,1,4};
+        for(int i=0;i< arr.length;i++){
+            int c=0;
+            for(int j=0;j< arr.length;j++){
+                if(arr[i]==arr[j]){
+                    c++;
+                }
+            }
+            if(c==1){
+                System.out.println(arr[i]);
+            }
+        }
+    }
+}
