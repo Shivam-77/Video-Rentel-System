b5977431f783582bec39fa0699c5dd612eac6407
Shivam-77 <tiwarishivam77777@gmail.com>
2022-08-06 23:41:31 +0530

    Add all the changes

--- src/Definitions/Video.java
+++ src/Definitions/Video.java
@@ -2,4 +2,26 @@ package Definitions;
 
 public class Video
 {
+//    Member variables
+
+    private String videoName;
+    private boolean checkOut;
+    private int rating;
+
+//    Member functions
+
+//    This method returns video name
+    public String getVideoName()
+    {
+        return videoName;
+    }
+
+//    this will set a value of checkout true or false.
+//    if true then customer has invented
+
+
+    public void setCheckOut(boolean checkOut)
+    {
+        this.checkOut = checkOut;
+    }
 }
