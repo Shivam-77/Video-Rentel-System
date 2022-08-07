43404e3288891f75d0c1b1f6bfe0a0d7a1c8b9fb
Shivam-77 <tiwarishivam77777@gmail.com>
2022-08-07 13:57:45 +0530

    Add Changes

--- src/Execution/Main.java
+++ src/Execution/Main.java
@@ -4,6 +4,9 @@ public class Main
 {
     public static void main(String[] args){
         Video vid=new Video("Bahubali",true,10);
+        Video vid1=new Video("Suryavanshi",false,6);
+        Video vid2=vid;
+
 //        vid.setVideoName("Bahubali:the beginning");
 //        vid.setCheckOut(true);
 //        vid.setRating(10);
@@ -11,6 +14,17 @@ public class Main
         System.out.println(vid.getRating());
         System.out.println(vid.isCheckOut());
         System.out.println(vid.toString());
+        if(vid.hashCode()==vid1.hashCode()){
+            System.out.println("Vid and vid1 are equals");
+        }
+        else if(vid.hashCode()==vid2.hashCode()){
+            System.out.println("Vid and vid2 are equals and vid and vid1 are not equals");
+        }
+        else{
+            System.out.println("No one objects are  Equals");
+        }
+
+
 
 
 
