55a48ac25e8f795c1886518d1368c8281c80c927
Shivam-77 <tiwarishivam77777@gmail.com>
2022-08-07 13:40:13 +0530

    Add Changes

--- src/Execution/Main.java
+++ src/Execution/Main.java
@@ -4,11 +4,12 @@ public class Main
 {
     public static void main(String[] args){
         Video vid=new Video("Bahubali",true,10);
-//        vid.setVideoName("Bahubali:the begeninig");
+//        vid.setVideoName("Bahubali:the beginning");
 //        vid.setCheckOut(true);
 //        vid.setRating(10);
         System.out.println(vid.getVideoName());
         System.out.println(vid.getRating());
+        System.out.println(vid.isCheckOut());
 
 
 
